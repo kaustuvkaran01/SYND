864cc18d99e3f995f8b77aad4361bda8dbc3f715
Hemant Taneja <hemanttaneja30@gmail.com>
2019-08-30 21:56:04 +0530

    Dashboard added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,97 @@
-import React, { Component } from 'react'
-import 'tachyons';
+import React, { Component } from "react";
+import "tachyons";
 import "../styles/Login.css";
 export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <div className="app1"></div>
+                <div className="app2"></div>
+                <div className="app3"></div>
+                <div
+                    class="container mt5 center"
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        marginTop: "15%",
+                        maxWidth: "20%",
+                        maxHeight: "100%",
+                        borderRadius: "5px"
+                    }}
+                >
+                    <div
+                        class=" pa4 black-80"
+                        style={{
+                            backgroundColor: "#EF5A5A",
+                            borderRadius: "35px"
+                        }}
+                    >
+                        <div className="form measure  center pv3 ph4">
+                            <div
+                                id="sign_up"
+                                class="ba b--transparent ph0 mh0 "
+                            >
+                                <legend
+                                    class="f2 fw5 ph0 mh0 "
+                                    style={{
+                                        display: "flex",
+                                        color: "#FEFFDF",
+                                        alignContent: "center",
+                                        justifyContent: "center",
+                                        borderRadius: "35px"
+                                    }}
+                                >
+                                    Login
+                                </legend>
+                                <div class="mt4">
+                                    <input
+                                        class="pa2 input-reset ba b--white bg-transparent hover-white w-100"
+                                        type="email"
+                                        name="email-address"
+                                        id="email-address"
+                                        placeholder="Username"
+                                        styles={{}}
+                                    />
+                                </div>
+                                <div class="mv3">
+                                    <input
+                                        class="b pa2 input-reset ba b--white bg-transparent hover-white w-100"
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        placeholder="Password"
+                                    />
+                                </div>
 
-  render() {
-    return (
-      <div>
-      <div className="app1"></div>
-      <div className="app2"></div>
-      <div className="app3"></div>
-      <div class="container mt5 center" style={{display:"flex", flexDirection:"column",marginTop:"15%",maxWidth:"20%", maxHeight:"100%", borderRadius:"5px"}}>
-        
-        <div class=" pa4 black-80" style={{backgroundColor:"#EF5A5A", borderRadius:"35px"}}>
-          <div className="form measure  center pv3 ph4">
-           <div id="sign_up" class="ba b--transparent ph0 mh0 ">
-             <legend class="f2 fw5 ph0 mh0 " style={{display:"flex", color:"#FEFFDF", alignContent:"center", justifyContent:"center", borderRadius:"35px"}}>Login</legend>
-             <div class="mt4">
-                
-                <input class="pa2 input-reset ba b--white bg-transparent hover-white w-100" type="email" name="email-address"  id="email-address" placeholder="Username" styles={{}} />
-             </div>
-             <div class="mv3">
-              <input class="b pa2 input-reset ba b--white bg-transparent hover-white w-100" type="password" name="password"  id="password" placeholder="Password" />
-             </div>
-            
-             <div class="">
-              <button class="button bg-transparent pv2 ph3 mt3 pointer grow" style={{border:"2px solid white",background:"white", color:"#7749F5"}}>Sign In</button>
-             </div>
-            <div class="lh-copy f6 mt3 ba2 b--white grow" style={{color:"white"}}>
-              <a href="#0" class="ba2 b--white grow" style={{color:"white"}}>Forgot your password?</a>
+                                <div class="">
+                                    <button
+                                        class="button bg-transparent pv2 ph3 mt3 pointer grow"
+                                        style={{
+                                            border: "2px solid white",
+                                            background: "white",
+                                            color: "#7749F5"
+                                        }}
+                                    >
+                                        Sign In
+                                    </button>
+                                </div>
+                                <div
+                                    class="lh-copy f6 mt3 ba2 b--white grow"
+                                    style={{ color: "white" }}
+                                >
+                                    <a
+                                        href="#0"
+                                        class="ba2 b--white grow"
+                                        style={{ color: "white" }}
+                                    >
+                                        Forgot your password?
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-
-      </div>
-      </div>
-      </div>  
-    )
-  }
+        );
+    }
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,163 @@
+import React, { Component } from "react";
+import "tachyons";
+import "../styles/Dashboard.css";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <div
+                    class="col1 fl w-300px vh-100 hw-20 tc"
+                    style={{ backgroundColor: "#ffe79a" }}
+                >
+                    <img
+                        src="https://www.pngrepo.com/png/131598/170/notepad.png"
+                        alt="..."
+                        class="w-25 h-15 tc pv4"
+                    />
+                    <div style={{ flex: 1, flexDirection: "column" }}>
+                        <div
+                            class="tl pl5"
+                            style={{
+                                width: 300,
+                                height: 75,
+                                color: "#EF5A5A"
+                            }}
+                        >
+                            <h2>DASHBOARD</h2>
+                        </div>
+                        <div
+                            class="tl pl5"
+                            style={{
+                                width: 300,
+                                height: 50,
+                                color: "#EF5A5A"
+                            }}
+                        >
+                            <h4>Delete Request</h4>
+                        </div>
+                        <div
+                            class="tl pl5"
+                            style={{
+                                width: 300,
+                                height: 50,
+                                color: "#EF5A5A"
+                            }}
+                        >
+                            <h4>Delete Agent</h4>
+                        </div>
+                        <div
+                            class="tl pl5"
+                            style={{
+                                width: 300,
+                                height: 50,
+                                color: "#EF5A5A"
+                            }}
+                        >
+                            <h5>Sign out</h5>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="col2 fl w-80 vh-100 tc"
+                    style={{ backgroundColor: "#FEFFDF" }}
+                >
+                    <div className="app1"></div>
+                    <div className="app2"></div>
+                    <div className="app3"></div>
+                    <div style={{ color: "#EF5A5A" }}>
+                        <h1>Table</h1>
+                    </div>
+                    <div class="pa3 table">
+                        <div class="overflow-auto">
+                            <table class="f6 w-100 mw8 center" cellspacing="0">
+                                <thead>
+                                    <tr class="stripe-dark">
+                                        <th class="fw6 tl pa3 bg-white">
+                                            Name
+                                        </th>
+                                        <th class="fw6 tl pa3 bg-white">
+                                            Username
+                                        </th>
+                                        <th class="fw6 tl pa3 bg-white">
+                                            Email
+                                        </th>
+                                        <th class="fw6 tl pa3 bg-white">ID</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="lh-copy">
+                                    <tr class="stripe-dark">
+                                        <td class="pa3 bg-light-gray">
+                                            Hassan Johnson
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            @hassan
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            hassan@companywithalongdomain.co
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            14419232532474
+                                        </td>
+                                    </tr>
+                                    <tr class="stripe-dark">
+                                        <td class="pa3 bg-light-gray">
+                                            Taral Hicks
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            @hicks
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            taral@companywithalongdomain.co
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            72326219423551
+                                        </td>
+                                    </tr>
+                                    <tr class="stripe-dark">
+                                        <td class="pa3 bg-light-gray">
+                                            Tyrin Turner
+                                        </td>
+                                        <td class="pa3 bg-light-gray">@tt</td>
+                                        <td class="pa3 bg-light-gray">
+                                            ty@companywithalongdomain.co
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            92325170324444
+                                        </td>
+                                    </tr>
+                                    <tr class="stripe-dark">
+                                        <td class="pa3 bg-light-gray">
+                                            Oliver Grant
+                                        </td>
+                                        <td class="pa3 bg-light-gray">@oli</td>
+                                        <td class="pa3 bg-light-gray">
+                                            oliverg@companywithalongdomain.co
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            71165170352909
+                                        </td>
+                                    </tr>
+                                    <tr class="stripe-dark">
+                                        <td class="pa3 bg-light-gray">
+                                            Dean Blanc
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            @deanblanc
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            dean@companywithalongdomain.co
+                                        </td>
+                                        <td class="pa3 bg-light-gray">
+                                            71865178111909
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
